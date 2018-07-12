bd6ba5266a12b062fa97950a44c8bc5487d4af53
Jupna21 <jupna21@yahoo.com>
2018-07-12 14:34:19 +0000

    automatically backed up by learn

--- one_direction_hash.rb
+++ one_direction_hash.rb
@@ -1,9 +1,21 @@
+#print each name and age of each member of one direction:
+# one_direction = {"Zayn" => 25, "Liam" => 24, "Louis" => 26, "Harry" => 24, "Niall" => 24}
+
+# def print_ages(group)
+#   group.each do |name,age|
+#     puts "#{name} is #{age} years old."
+#   end
+# end
+
+# print_ages(one_direction)
+
+#how to get the average age of all the members in one one_direction:
+
 one_direction = {"Zayn" => 25, "Liam" => 24, "Louis" => 26, "Harry" => 24, "Niall" => 24}
+sum = 0
 
-def print_ages(group)
-  group.each do |name,age|
-    puts "#{name} is #{age} years old."
-  end
+one_direction.each do |name,age|
+  sum += age
 end
-
-print_ages(one_direction)
+average = sum / one_direction.count
+puts average
