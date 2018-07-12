89a2efc11e1ab995710192b449baf5245c12f52e
Jupna21 <jupna21@yahoo.com>
2018-07-12 14:11:04 +0000

    automatically backed up by learn

--- capitals_challenge.rb
+++ capitals_challenge.rb
@@ -4,16 +4,18 @@ capitals = {"MA" => "Boston", "WI" => "Madison", "NJ" => "Trenton", "CO" => "Den
 
 #1. Access postal code of Wisconsin
 puts states["Wisonsin"]
+
 #2. Access capital of NJ
 capitals["NJ"]
+
 #3. Add Ohio => OH to states hash
 states["Ohio"]= "OH"
 
-#4. Iterate through and print all states and postal codes
-# puts "#{states} #{codes}"
-states.each do |states,codes|
+# #4. Iterate through and print all states and postal codes
+# # puts "#{states} #{codes}"
+# states.each do |states,codes|
   
-#5. Iterate through states hash and print just states
-# puts capitals.each_value { |capitals| puts capitals }
+# #5. Iterate through states hash and print just states
+# # puts capitals.each_value { |capitals| puts capitals }
 
-#6. Iterate through capitals and print just capitals
+# #6. Iterate through capitals and print just capitals
